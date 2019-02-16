683fb313a7545b5d9efc6b6ac774963895fc9416
TsungJui <tsungjui@TsungJuide-MacBook-Pro.local>
2019-02-16 17:54:11 +0800

    Connect to MongoDB

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 from pymodm.connection import connect
-
+from pymodm import MongoModel, fields
 # Request handler
 def handler(sock,addr):
     msg = 'YEE from server. YEEEEEEEEEE'
@@ -21,6 +21,10 @@ def handler(sock,addr):
     msg = 'Closing connection...'
     sock.send(msg.encode('utf-8'))
     sock.close()
+class User(MongoModel):
+        email = fields.EmailField(primary_key=True)
+        class Meta:
+            connection_alias = 'myServer'
 
 if __name__ == '__main__':
     # Turn on server
@@ -28,7 +32,9 @@ if __name__ == '__main__':
     sock.bind(('0.0.0.0', 8080))# port
     sock.listen(5)
     # Connect to mongodb
-    connect("mongodb://mongo:27017/test")
+    connect("mongodb://mongo:27017/test", alias="myServer")
+    # testing for insertion
+    User("j2081499@gmail.com").save()
     print('Waiting for connection...')
     while True:
         (socket,addr) = sock.accept()
